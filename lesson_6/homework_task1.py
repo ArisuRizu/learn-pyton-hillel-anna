1808eb86253eca2809b9e47e6443db7b565d42f8
arisu <annyuiz@ukr.net>
2023-06-18 17:09:37 +0300

    Homework lesson 7

--- lesson_6/homework_task1.py
+++ lesson_6/homework_task1.py
@@ -7,6 +7,10 @@ user_input = input('Для перевірки моєї серйозності м
 # Якщо строка має дужку то ми видаляємо її разом з текстом всередині, цикл повторюється.
 while '(' in user_input:
     user_input = user_input[:user_input.index('(')] + user_input[user_input.index(')') + 1:]
+    else:
+        break
+
+user_input = user_input.replace('(', '').replace(')', '')
 
 print(user_input)
 print('Так краще!')
